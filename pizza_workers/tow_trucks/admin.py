7ecd45d2a15ffca0849ea092dd259829f21cd0f4
AntonL <sa-fa@mail.ru>
2023-04-27 18:39:25 +0300

    [upd]  10 commit -> Make new migrations

--- pizza_workers/tow_trucks/admin.py
+++ pizza_workers/tow_trucks/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Driver, Doc, Car, Garage
 
-# Register your models here.
+admin.site.register(Doc)
+admin.site.register(Garage)
+admin.site.register(Driver)
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ['name', 'age', 'document', 'garage']
+    list_editable = ['document', 'garage']
+    filter_horizontal = ['drivers']
--- pizza_workers/tow_trucks/models.py
+++ pizza_workers/tow_trucks/models.py
@@ -7,21 +7,33 @@ class Doc(models.Model):
     age = models.IntegerField(validators=[MinValueValidator(2010), MaxValueValidator(2023)])
     number = models.CharField(max_length=5, default='00000')
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Driver(models.Model):
     name = models.CharField(max_length=20)
     year = models.IntegerField(validators=[MinValueValidator(18), MaxValueValidator(70)])
     experience = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(50)])
 
+    def __str__(self):
+        return f'{self.name}'
 
-class Spares(models.Model):
+
+class Garage(models.Model):
     name = models.CharField(max_length=20)
-    price = models.IntegerField()
+    size_box = models.IntegerField(validators=[MinValueValidator(20), MaxValueValidator(100)])
+
+    def __str__(self):
+        return f'{self.name}'
 
 
 class Car(models.Model):
     name = models.CharField(max_length=20)
     age = models.IntegerField(validators=[MinValueValidator(2010), MaxValueValidator(2023)])
     document = models.OneToOneField(Doc, on_delete=models.CASCADE)
-    drivers = models.ManyToManyField(Driver)
-    spares = models.ForeignKey(Spares, on_delete=models.CASCADE, null=True)
+    drivers = models.ManyToManyField(Driver, blank=True)
+    garage = models.ForeignKey(Garage, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
