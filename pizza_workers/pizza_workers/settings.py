1ed30d6310f89c20401c7f77a9521aa903712823
AntonL <sa-fa@mail.ru>
2023-04-28 16:56:49 +0300

    [upd]  14 commit -> add new app Product and add model in Product.models

--- pizza_workers/pizza_workers/settings.py
+++ pizza_workers/pizza_workers/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'workers',
     'tow_trucks.apps.TowTrucksConfig',
+    'product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +118,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
---
+++ pizza_workers/product/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.IntegerField(null=True, blank=True)
+    quantity = models.IntegerField()
+    doc = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        pass
